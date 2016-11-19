25be1e41e4b501055f54bfb8fafc09cd2e414d10
Joel Lopez <jowie.jowie@gmail.com>
2016-11-19 23:28:42 +0000

    Add bullet firing and collisions

--- src/host/index.js
+++ src/host/index.js
@@ -11,12 +11,15 @@ const game = new Phaser.Game(
   { preload, create, update, render }
 )
 
-let playerGroup, cursors
-let players
+let playerGroup, cursors, bulletGroup
+let players, bullets
+let fire = false
+let fireKey
 
 function preload () {
   // asset loading stuff goes here
   game.load.image('wizard', 'images/wizardSmall.png')
+  game.load.image('bullet5', 'images/bullet.png')
 }
 
 function create () {
@@ -44,19 +47,61 @@ function create () {
     players.push(player)
   }
 
+  bullets = []
+  bulletGroup = game.add.group()
+  bulletGroup.enableBody = true
+  bulletGroup.physicsBodyType = Phaser.Physics.ARCADE
+
   cursors = game.input.keyboard.createCursorKeys()
   game.input.gamepad.start()
+
+  fireKey = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR)
+  fireKey.onUp.add(function () {
+    fire = true
+  })
 }
 
 const SPEED = 200
 function update () {
   game.physics.arcade.collide(playerGroup)
+  game.physics.arcade.collide(playerGroup, bulletGroup, bulletCollided)
 
   const player = players[0]
   player.body.velocity.x = SPEED * Number(cursors.right.isDown) - SPEED * Number(cursors.left.isDown)
   player.body.velocity.y = SPEED * Number(cursors.down.isDown) - SPEED * Number(cursors.up.isDown)
+
+  if (fire) {
+    fire = false
+    fireBullet(player)
+  }
+}
+
+function fireBullet (player) {
+  const bullet = bulletGroup.create(
+    player.x, player.y + 100,
+    'bullet5'
+  )
+  // bullet.anchor.x = 0.5
+  bullet.body.setSize(10, 10, 0, 0)
+  bullet.body.collideWorldBounds = true
+  bullet.body.velocity.y = 200
+  bullets.push(bullet)
+}
+
+function bulletCollided (player, bullet) {
+  playerGroup.remove(player, true)
+  bulletGroup.remove(bullet, true)
+  console.log('Bullet collided with ' + player)
 }
 
 function render () {
   players.forEach(player => game.debug.body(player))
+  bullets.forEach(bullet => game.debug.body(bullet))
 }
+
+function resize () {
+  game.scale.setGameSize(window.innerWidth, window.innerHeight)
+  game.scale.refresh()
+}
+
+window.onresize = resize
