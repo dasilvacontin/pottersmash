a2eb3a4ea3cdd011178277ca7a76e41407863618
David da Silva <dasilvacontin@gmail.com>
2016-11-19 19:20:46 +0000

    Fix lint issues

--- src/mapGenerator.js
+++ src/mapGenerator.js
@@ -1,12 +1,12 @@
-exports.getMap = function (rows=5, cols=5) {
+exports.getMap = function (rows = 5, cols = 5) {
   var map = [0]
   var i, j
 
-  for (i = 0; i < rows; i++){
+  for (i = 0; i < rows; i++) {
     map[i] = []
-    for (j = 0; j < cols; j++){
+    for (j = 0; j < cols; j++) {
       map[i][j] = 0
     }
   }
-  return map                 
+  return map
 }
