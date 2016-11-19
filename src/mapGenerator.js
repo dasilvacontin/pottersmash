0a4f6decad106480c94e33940e10b708e36e6e5c
Alice <alicenara165@gmail.com>
2016-11-19 19:54:52 +0100

    map with all 0

---
+++ src/mapGenerator.js
@@ -0,0 +1,12 @@
+exports.getMap = function (rows=5, cols=5) {
+  var map = [0]
+  var i, j
+
+  for (i = 0; i < rows; i++){
+    map[i] = []
+    for (j = 0; j < cols; j++){
+      map[i][j] = 0
+    }
+  }
+  return map                 
+}
