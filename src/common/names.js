4d8927e736121e791a8a29c8642d2dba31ef9614
Ying <yixuli2794@gmail.com>
2016-11-20 04:18:12 +0000

    Added javascript names file

---
+++ src/common/names.js
@@ -0,0 +1,46 @@
+module.export = {
+  'Griffindor': [
+    'Lily Evans',
+    'Hermione Granger',
+    'Neville Longbottom',
+    'Dean Thomas',
+    'Seamus Finnigan',
+    'Lavender Brown',
+    'Cormac McLaggen',
+    'Parvati Patil',
+    'Albus Dumbledore'
+  ],
+  'Hufflepuff': [
+    'Pomona Sprout',
+    'Nymphadora Tonks',
+    'Cedric Diggory',
+    'Ernie Macmillan',
+    'Hannah Abbott',
+    'Justin Finch-Fletchley',
+    'Susan Bones',
+    'Zacharias Smith',
+    'Fraile Gordo'
+  ],
+  'Ravenclaw': [
+    'Luna Lovegood',
+    'Sybill Trelawney',
+    'Cho Chang',
+    'Myrtle la Llorona',
+    'Padma Patil',
+    'Roger Davies',
+    'Terry Boot',
+    'Michael Corner',
+    'Anthony Goldstein'
+  ],
+  'Slytherin': [
+    'Tom Sorvolo Ryddle',
+    'Severus Snape',
+    'Bellatrix Lestrange',
+    'Regulus Black',
+    'Horace Slughorn',
+    'Andromeda Tonks',
+    'Phineas Nigellus Black',
+    'Barón Sanguinario',
+    'Draco Malfoy']
+}
+
