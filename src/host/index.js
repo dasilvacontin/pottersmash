dca99016ae4d1708545c654eaccc641f35ca2a43
Alice <alicenara165@gmail.com>
2016-11-20 02:50:05 +0100

    change fire x y

--- src/host/index.js
+++ src/host/index.js
@@ -11,10 +11,12 @@ const game = new Phaser.Game(
   { preload, create, update, render }
 )
 
-let playerGroup, cursors, bulletGroup
+let playerGroup, cursors, keys, bulletGroup
 let players, bullets
-let fire = false
-let fireKey
+// let fire = false
+// let fireKey
+let nextFire
+const FIRERATE = 100
 
 function preload () {
   // asset loading stuff goes here
@@ -53,38 +55,53 @@ function create () {
   bulletGroup.physicsBodyType = Phaser.Physics.ARCADE
 
   cursors = game.input.keyboard.createCursorKeys()
+  keys = {
+    up: game.input.keyboard.addKey(Phaser.KeyCode.W),
+    left: game.input.keyboard.addKey(Phaser.KeyCode.A),
+    down: game.input.keyboard.addKey(Phaser.KeyCode.S),
+    right: game.input.keyboard.addKey(Phaser.KeyCode.D)
+  }
   game.input.gamepad.start()
 
-  fireKey = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR)
-  fireKey.onUp.add(function () {
-    fire = true
-  })
+  nextFire = game.time.now + FIRERATE
+
+  // fireKey = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR)
+  // cursors.isUp.add(function () {
+    // fire = true
+  // })
 }
 
 const SPEED = 200
+const FSPEED = 200
+
 function update () {
   game.physics.arcade.collide(playerGroup)
   game.physics.arcade.collide(playerGroup, bulletGroup, bulletCollided)
 
   const player = players[0]
-  player.body.velocity.x = SPEED * Number(cursors.right.isDown) - SPEED * Number(cursors.left.isDown)
-  player.body.velocity.y = SPEED * Number(cursors.down.isDown) - SPEED * Number(cursors.up.isDown)
+  player.body.velocity.x = SPEED * Number(keys.right.isDown) - SPEED * Number(keys.left.isDown)
+  player.body.velocity.y = SPEED * Number(keys.down.isDown) - SPEED * Number(keys.up.isDown)
 
-  if (fire) {
-    fire = false
-    fireBullet(player)
+  if (game.time.now > nextFire) {
+    let fx = Number(cursors.right.isDown) - Number(cursors.left.isDown)
+    let fy = Number(cursors.down.isDown) - Number(cursors.up.isDown)
+    if (fx !== 0 || fy !== 0) {
+      fireBullet(player, fx, fy)
+      nextFire = game.time.now + FIRERATE
+    }
   }
 }
 
-function fireBullet (player) {
+function fireBullet (player, x, y) {
   const bullet = bulletGroup.create(
-    player.x, player.y + 100,
+    player.x + 50 * x, player.y + 50 + (y < 0 ? 60 : 50) * y,
     'bullet5'
   )
   // bullet.anchor.x = 0.5
   bullet.body.setSize(10, 10, 0, 0)
   bullet.body.collideWorldBounds = true
-  bullet.body.velocity.y = 200
+  bullet.body.velocity.y = FSPEED * y
+  bullet.body.velocity.x = FSPEED * x
   bullets.push(bullet)
 }
 
