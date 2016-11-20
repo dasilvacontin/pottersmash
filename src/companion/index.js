7f6102d120fc335d3b1c076069c8bb0233ea3ae0
David da Silva <dasilvacontin@gmail.com>
2016-11-20 01:10:42 +0000

    Add touch handling to controller
    Closes #2.

--- src/companion/index.js
+++ src/companion/index.js
@@ -1,31 +1,56 @@
 /* globals screen Element */
 // companion main file
+
+const { Joystick } = require('./joystick.js')
+var canvas = document.getElementById('player-controller')
 console.log('Im the companion app!')
 
-function drawCircles (canvas) {
+const elements = [
+  new Joystick(0, 0),
+  new Joystick(0, 0)
+]
+
+function updateJoysticks () {
   var ctx = canvas.getContext('2d')
   var radius = canvas.height * 0.30
 
+  let centerx, centery
+  centerx = canvas.width - (canvas.width / 4)
+  centery = canvas.height / 2
   ctx.beginPath()
-  ctx.arc(canvas.width - (canvas.width / 4), canvas.height / 2, radius, 0, 2 * Math.PI)
+  ctx.arc(centerx, centery, radius, 0, 2 * Math.PI)
   ctx.stroke()
+  elements[0].updateCenter(centerx, centery)
 
+  centerx = canvas.width / 4
+  centery = canvas.height / 2
   ctx.beginPath()
-  ctx.arc(canvas.width / 4, canvas.height / 2, radius, 0, 2 * Math.PI)
+  ctx.arc(centerx, centery, radius, 0, 2 * Math.PI)
   ctx.stroke()
+  elements[1].updateCenter(centerx, centery)
 }
 
 function resized () {
-  // Setup canvas size
-  var orientation = screen.orientation || screen.mozOrientation || screen.msOrientation
-  if (orientation.type === 'landscape-primary' || orientation.type === 'landscape-secondary') {
-    window.onclick = function () {
-      requestFullscreen(document.getElementById('controllers'))
+  // setTimeout since Safari doesn't update window.orientation before firing the resize event
+  setTimeout(function () {
+    // Setup canvas size
+    let orientation = screen.orientation || screen.mozOrientation || screen.msOrientation
+    if (orientation == null) {
+      orientation = Math.abs(window.orientation) === 90
+        ? { type: 'landscape-primary' }
+        : { type: 'portrait' }
     }
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight
-    drawCircles(canvas)
-  }
+    console.log(orientation)
+    if (orientation.type === 'landscape-primary' || orientation.type === 'landscape-secondary') {
+      window.onclick = function () {
+        requestFullscreen(document.getElementById('controllers'))
+      }
+      canvas.width = window.innerWidth
+      canvas.height = window.innerHeight
+      console.log(window.innerWidth, window.innerHeight)
+      updateJoysticks(canvas)
+    }
+  }, 1)
 }
 
 function requestFullscreen (element) {
@@ -41,5 +66,66 @@ function requestFullscreen (element) {
 window.onresize = resized
 // requestFullscreen(window.document.documentElement)
 
-var canvas = document.getElementById('player-controller')
 resized()
+
+window.addEventListener('touchstart', touchStart)
+window.addEventListener('touchmove', touchMove)
+window.addEventListener('touchend', touchEnd)
+
+const touchToElement = {}
+
+function touchIsAssigned (touch) {
+  return Boolean(touchToElement[touch.identifier])
+}
+
+function touchStart (e) {
+  const touches = Array.from(e.changedTouches)
+  const unassignedTouches = touches.filter(touch => !touchIsAssigned(touch))
+
+  for (let touch of unassignedTouches) {
+    let closestElement = null
+    let minDistance = Infinity
+    for (let element of elements) {
+      let distance = element.distanceToTouch(touch)
+      if (distance < minDistance) {
+        minDistance = distance
+        closestElement = element
+      }
+    }
+    if (!closestElement.hasTouchAssigned()) {
+      closestElement.assignTouch(touch)
+      touchToElement[touch.identifier] = closestElement
+    }
+  }
+
+  touchMove(e)
+  e.preventDefault()
+}
+
+function touchMove (e) {
+  const touches = Array.from(e.changedTouches)
+  for (let touch of touches) {
+    const element = touchToElement[touch.identifier]
+    if (!element) continue
+    element.processTouchMove(touch)
+
+    const ctx = canvas.getContext('2d')
+    // debug
+    ctx.beginPath()
+    ctx.moveTo(element.centerx, element.centery)
+    ctx.lineTo(touch.pageX, touch.pageY)
+    ctx.stroke()
+  }
+  e.preventDefault()
+}
+
+function touchEnd (e) {
+  const touches = Array.from(e.changedTouches)
+  for (let touch of touches) {
+    const element = touchToElement[touch.identifier]
+    if (!element) continue
+    delete touchToElement[touch.identifier]
+    element.assignTouch(null)
+  }
+  e.preventDefault()
+}
